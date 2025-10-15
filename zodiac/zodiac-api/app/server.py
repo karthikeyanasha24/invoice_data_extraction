5c81efd2ec77ee75094121b2f0811456ca2dc1c6
Zack Mwangi <zackmwangi@gmail.com>
2025-10-15 23:02:46 +0300

    adding vercel config

--- zodiac/zodiac-api/app/server.py
+++ zodiac/zodiac-api/app/server.py
@@ -58,8 +58,9 @@ logger.info(f"🌐 CORS origins configured: {origins}")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    #allow_origins=origins,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
